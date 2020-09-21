717b168f663c83dcec6c1c88dd48ba02c84405aa
Khanh-Nobi-JR <69251285+Khanh-Nobi-JR@users.noreply.github.com>
2020-09-21 16:52:46 +0700

    add

--- src/CalculatorFizzBuzz.java
+++ src/CalculatorFizzBuzz.java
@@ -1,15 +1,22 @@
 public class CalculatorFizzBuzz {
     public static String checkFizzBuzz(int number){
 
-        if (number % 3 == 0 && number % 5 == 0){
+        boolean isFizzBuzz = number % 3 == 0 && number % 5 == 0;
+        if (isFizzBuzz){
             return "fizzBuzz";
         }
-        else if (number % 3 == 0){
-            return "fizz";
-        } else if (number % 5 == 0){
-            return "buzz";
-        }else
-           return readNumber(number);
+        else {
+            boolean isFizz = number % 3 == 0;
+            if (isFizz){
+                return "fizz";
+            } else {
+                boolean isBuzz = number % 5 == 0;
+                if (isBuzz){
+                    return "buzz";
+                }else
+                   return readNumber(number);
+            }
+        }
 
     }
     public static String readNumber(int number){
@@ -19,80 +26,75 @@ public class CalculatorFizzBuzz {
             String readUnit = "";
             int tens = number / 10;
             String readTens =  "";
-            if (unit == 3 || tens ==3 && unit == 5 || tens ==5){
+            boolean isFizzBuzz = unit == 3 || tens == 3 && unit == 5 || tens == 5;
+            if (isFizzBuzz){
                 return "fizzBuzz";
             }
-            else if (unit == 3 || tens ==3){
-                return "fizz";
-            } else if (unit == 5 || tens ==5){
-                return "buzz";
-            } else {
-                switch (unit) {
-                    case 0:
-                        readUnit = "";
-                        break;
-                    case 1:
-                        readUnit = " mot";
-                        break;
-                    case 2:
-                        readUnit = " hai";
-                        break;
-                    case 3:
-                        readUnit = " ba";
-                        break;
-                    case 4:
-                        readUnit = " bon";
-                        break;
-                    case 5:
-                        readUnit = " nam";
-                        break;
-                    case 6:
-                        readUnit = "sau";
-                        break;
-                    case 7:
-                        readUnit = " bay";
-                        break;
-                    case 8:
-                        readUnit = " tam";
-                        break;
-                    case 9:
-                        readUnit = " chin";
-                        break;
-                }
-                switch (tens) {
-                    case 0:
-                        readTens = "";
-                        break;
-                    case 1:
-                        readTens = " mot";
-                        break;
-                    case 2:
-                        readTens = " hai";
-                        break;
-                    case 3:
-                        readTens = " ba";
-                        break;
-                    case 4:
-                        readTens = " bon";
-                        break;
-                    case 5:
-                        readTens = " nam";
-                        break;
-                    case 6:
-                        readTens = "sau";
-                        break;
-                    case 7:
-                        readTens = " bay";
-                        break;
-                    case 8:
-                        readTens = " tam";
-                        break;
-                    case 9:
-                        readTens = " chin";
-                        break;
+            else {
+                boolean isFizz = unit == 3 || tens == 3;
+                if (isFizz){
+                    return "fizz";
+                } else {
+                    boolean isBuzz = unit == 5 || tens == 5;
+                    if (isBuzz){
+                        return "buzz";
+                    } else {
+                        switch (unit) {
+                            case 0:
+                                readUnit = "";
+                                break;
+                            case 1:
+                                readUnit = " mot";
+                                break;
+                            case 2:
+                                readUnit = " hai";
+                                break;
+                            case 4:
+                                readUnit = " bon";
+                                break;
+                            case 6:
+                                readUnit = "sau";
+                                break;
+                            case 7:
+                                readUnit = " bay";
+                                break;
+                            case 8:
+                                readUnit = " tam";
+                                break;
+                            case 9:
+                                readUnit = " chin";
+                                break;
+                        }
+                        switch (tens) {
+                            case 0:
+                                readTens = "";
+                                break;
+                            case 1:
+                                readTens = " mot";
+                                break;
+                            case 2:
+                                readTens = " hai";
+                                break;
+                            case 4:
+                                readTens = " bon";
+                                break;
+                            case 6:
+                                readTens = "sau";
+                                break;
+                            case 7:
+                                readTens = " bay";
+                                break;
+                            case 8:
+                                readTens = " tam";
+                                break;
+                            case 9:
+                                readTens = " chin";
+                                break;
 
+                        }
+                        return readTens + readUnit;
+                    }
                 }
-                return readTens + readUnit;
             }
         }
         return "";
